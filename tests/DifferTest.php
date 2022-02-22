6718a44e4107cbf4f6311f3c9fb7aaa308c26345
Dmitry <ddm14159@gmail.com>
2022-02-22 23:23:14 +0300

    add test

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -2,16 +2,6 @@
 
 namespace Differ\Tests;
 
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-// Путь для локальной работы с проектом
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
-
 use PHPUnit\Framework\TestCase;
 
 use function Differ\Differ\genDiff;
