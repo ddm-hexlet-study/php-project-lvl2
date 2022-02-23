193837425e8eb28a141dc7a7c539d0c6be882274
Dmitry <ddm14159@gmail.com>
2022-02-23 15:58:19 +0300

    codeclimate

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -12,7 +12,7 @@ class DifferTest extends TestCase
 
     public function setUp(): void
     {
-          $this->expected = "{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}\n";
+          $this->expected = "{\n  - follow: false\n host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}\n";
     }
     public function testDiff(): void
     {
